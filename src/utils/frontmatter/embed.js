0526f0df739a5eb84095b0ffb44f17b2594ae3db
duong tam kien <tk@renard.ninja>
2026-01-14 12:32:42 +0100

    import embed from frontmatter parser

---
+++ src/utils/frontmatter/embed.js
@@ -0,0 +1,47 @@
+import { doc, util } from 'prettier';
+// import inferParser from '../../utilities/infer-parser.js';
+import isFrontMatter from './is-front-matter.js';
+
+const SUPPORTED_EMBED_LANGUAGES = new Set(['yaml', 'toml']);
+
+const hardline = doc.builders.hardline;
+const markAsRoot = doc.builders.markAsRoot;
+const inferParser = util.inferParser;
+
+const isEmbedFrontMatter = ({ node } /* , options */) =>
+  isFrontMatter(node) && SUPPORTED_EMBED_LANGUAGES.has(node.language);
+
+async function printEmbedFrontMatter(textToDoc, print, path, options) {
+  const { node } = path;
+  const { language } = node;
+
+  if (!SUPPORTED_EMBED_LANGUAGES.has(language)) {
+    return;
+  }
+
+  const value = node.value.trim();
+
+  let doc;
+  if (value) {
+    const parser =
+      language === 'yaml' ? language : inferParser(options, { language });
+    if (!parser) {
+      return;
+    }
+
+    doc = value ? await textToDoc(value, { parser }) : '';
+  } else {
+    doc = value;
+  }
+
+  return markAsRoot([
+    node.startDelimiter,
+    node.explicitLanguage ?? '',
+    hardline,
+    doc,
+    doc ? hardline : '',
+    node.endDelimiter,
+  ]);
+}
+
+export { isEmbedFrontMatter, printEmbedFrontMatter };
