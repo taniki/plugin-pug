eb2a82ac164d53aa1a2e17a3b767c67daa810a7a
duong tam kien <tk@renard.ninja>
2026-01-14 01:18:12 +0100

    use import instead of global variable

--- tests/frontmatter/frontmatter.test.ts
+++ tests/frontmatter/frontmatter.test.ts
@@ -2,8 +2,8 @@ import { compareFiles } from 'tests/common';
 import { describe, expect, it } from 'vitest';
 
 describe('Frontmatter', () => {
-  it('should format yaml frontmatter', () => {
-    const { actual, expected } = compareFiles(__dirname);
+  it('should format yaml frontmatter', async () => {
+    const { expected, actual } = await compareFiles(import.meta.url);
     expect(actual).toBe(expected);
   });
 });
